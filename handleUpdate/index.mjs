7546646195e304a87fd132038d66ac188c026f0c
KaedenOC <komeacarr2@gmail.com>
2023-06-09 09:55:01 -0700

    handleupdate functioning correctly

--- handleUpdate/index.mjs
+++ handleUpdate/index.mjs
@@ -1,3 +1,35 @@
 'use strict';
 
+import dynamoose from 'dynamoose';
+
+const schema = new dynamoose.Schema({
+  "id": String,
+  "name": String,
+  "phone": String
+});
+
+const peopleModel = dynamoose.model("people", schema);
+
+export const handler = async (event) => {
+  console.log('this is event', event.body);
+  // TODO implement
+
+  const response = { statusCode: null, body: null, };
+  try {
+    let id = event?.pathParameters?.id;
+    let parsedBody = JSON.parse(event.body);
+    let result = await peopleModel.update({ id }, parsedBody);
+    console.log('update by id', result);
+    response.body = JSON.stringify(result);
+    response.statusCode = 200;
+  } catch (error) {
+    response.body = JSON.stringify(error.message);
+    response.statusCode = 500;
+  }
+
+  return response;
+};
+
+
+
 
